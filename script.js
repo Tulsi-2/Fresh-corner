859a415ec2e99968e2f498c65bfd67a910868d01
Tulsi-2 <MY_NAME@example.com>
2023-08-29 21:54:18 +0530

    I have add typed js

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ const contact =()=>{
     // console.log(name,phone,email,msg )
     alert("Your msg has been sent")
 }
+
+var typed = new Typed(".auto-type",{
+    strings : ["Fresh Fruits And Vegetables ........   For You!"],
+    
+    typespeed : 200,
+    backspeed : 200,
+    looped : true
+})
