848e9eff05bb792988d19c54fa63b4b75debcd61
Tulsi-2 <MY_NAME@example.com>
2023-08-29 12:33:16 +0530

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const contact =()=>{
+    let name = document.getElementById("text").innerHTML;
+    let phone = document.getElementById("phone").innerHTML;
+    let email = document.getElementById("email").innerHTML;
+    let msg = document.getElementById("textarea").innerHTML;
+    // console.log(name,phone,email,msg )
+    alert("Your msg has been sent")
+}
